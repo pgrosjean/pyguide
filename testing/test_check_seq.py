dc75a2d085d92eeb745da4eacf08ad5b20e541ea
pgrosjean <parker.grosjean@gmail.com>
2022-12-15 15:37:36 -0800

    Fixed type error for pytest coverage, which only occurs on linux systems.

--- testing/test_check_seq.py
+++ testing/test_check_seq.py
@@ -13,6 +13,6 @@ def test_check_seq():
     file_path_1 = os.path.realpath(os.path.join(os.getcwd(), os.path.dirname(__file__)))
     file_path_2 = os.path.join(file_path_1, "example", "seq_files")
     collated_files = check_seq.collate_files(file_path_2, ".seq")
-    guide_ids = sorted(check_seq.check_seq(collated_files, "human", "i"))
+    guide_ids = sorted(check_seq.check_seq(collated_files, "human", "i").astype(str))
     assert guide_ids[0] == 'ASB1_-_239335717.23-P1P2'
     assert guide_ids[-1] == 'SOCS6_+_67956710.23-P1P2'
